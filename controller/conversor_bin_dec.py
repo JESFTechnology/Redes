c5e049baab6d6ee5c7219040da5734546837f7b5
Johann Estevão Sacconi Ferreira <johannsacconi.jesftech@gmail.com>
2026-05-04 08:06:39 -0300

    Remove binary conversion scripts for cleaner codebase

--- controller/conversor_bin_dec.py
+++
@@ -1,8 +0,0 @@
-ip = "10101100.00010000.00000001.00000001"
-novo_ip = ""
-for octeto in ip.split("."):
-    decimal = 0
-    for i in range(8):
-        decimal += int(octeto[i]) * (2 ** (7 - i))
-    novo_ip += str(decimal) + "."
-print(novo_ip[:-1])
